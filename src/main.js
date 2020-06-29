a512c14e894c0801d520334cfc6d8110f184d1a3
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-29 16:17:15 -0700

    add front end logic
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,33 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { Animal } from './animals';
 
-$(document).ready(function() {
+
+function updateAnimalNeeds(animal) {
+  setInterval(() => {
+    $("#food-level").text(animal.foodLevel);
+    $("#water-level").text(animal.waterLevel);
+    $("#energy").text(animal.energy);
+  }, 1000);
+}
+
+
+
+$(document).ready(function () {
+  let animal = new Animal();
+  animal.hunger();
+  animal.thirst();
+  animal.fatigue();
+  animal.isAlive();
+  updateAnimalNeeds(animal);
+
+  $("#feed").click(function () {
+    event.preventDefault();
+    animal.feed();
+    $("#food-level").text(animal.foodLevel);
+  });
 
 });
+
+
